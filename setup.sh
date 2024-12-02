6c87c25d892d6acbdeddaf51bfcdf4bbcedd50a9
deepakgajula <gajuladeepak17@gmail.com>
2024-12-02 17:28:24 +0530

    k8-resources

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#kubectl install
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.2/2024-11-15/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+
+
+#eksctl install
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
